af624314a760582fefd41579b9aae2fbbf4536ed
VorapratR <vorapratrattana@gmail.com>
2019-03-21 18:05:11 +0700

    ตกแต่ง

--- src/App.js
+++ src/App.js
@@ -2,42 +2,55 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import _ from 'lodash';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
 const URL = 'https://swapi.co/api/starships/';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state = { 
-      starships: [] 
+    this.state = {
+      starships: []
     }
   }
-  
+
   componentDidMount() {
     axios.get(URL)
       .then(res => {
-        this.setState( {starships: res.data.results})
+        this.setState({ starships: res.data.results })
         console.log(res.data.results)
       })
   }
-  renderDisplay(){
-    return _.map(this.state.starships,starship =>{
-      return(
-        <div>
-            <li className = "list-group-item">
-              {starship.name} + {starship.model}
-            </li>
+  renderDisplay() {
+    return _.map(this.state.starships, starship => {
+      return (
+        <div >
+          <h2>name: {starship.name}</h2>
+          <li className="list-group-item">
+            model: {starship.model}<br/>
+          </li>
+          <li className="list-group-item">
+           manufacturer: {starship.manufacturer}<br/>
+          </li>
+          <li className="list-group-item">
+            length: {starship.length}<br/>
+          </li>
+          <li className="list-group-item">
+            max_atmosphering_speed: {starship.max_atmosphering_speed}<br/>
+          </li>
         </div>
-        
       )
     })
   }
 
   render() {
     return (
-      <div style={{background: "#FF0000"}}>
-          <h1>starship</h1>
+      <MuiThemeProvider>
+        <div>
+          <AppBar title="starship" />
           {this.renderDisplay()}
-      </div>
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
